1a1d41209c38f140b3c99f37175af7ea53b965c3
Mumin Cacao <mumincacao@gmail.com>
2025-10-04 22:48:52 +0900

    Add minifest settings.

--- wxt.config.ts
+++ wxt.config.ts
@@ -13,5 +13,13 @@ export default defineConfig({
     description: 'Simple request block/replace extension for firefox.',
     version: '0.0.1',
     permissions: ['webRequest', 'webRequestBlocking', '<all_urls>', 'storage'],
+    browser_specific_settings: {
+      gecko: {
+        data_collection_permissions: {
+          required: ["none"],
+        },
+      },
+    },
+    homepage_url: 'https://github.com/mumincacao/replock',
   }
 });
